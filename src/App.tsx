9dd6cf279f375c5c4140163d494067fdd61c7dfc
Jjiya <gpwl9610@naver.com>
2022-04-28 16:20:20 +0900

    react typescript - nest typescript connection complete

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,24 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
 function App() {
+  const [apiTest, setApiTest] = useState("api 통신 중...");
+
+  useEffect(() => {
+    const getHello = async() => {
+      const response =  await axios.get("/api");
+      setApiTest(response.data);
+    }
+    getHello();
+  }, []);
+  
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
+        <p> {apiTest} </p>
         <a
           className="App-link"
           href="https://reactjs.org"

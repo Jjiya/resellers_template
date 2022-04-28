8e04033fc7b9cca6215dbda6141c085372eec626
Jjiya <gpwl9610@naver.com>
2022-04-28 17:21:16 +0900

    nestJs connection Swagger

--- server/src/app.controller.ts
+++ server/src/app.controller.ts
@@ -1,11 +1,15 @@
 import { Controller, Get } from '@nestjs/common';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { AppService } from './app.service';
 
-@Controller('/api')
+@Controller()
+@ApiTags('Index')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
+  @ApiOperation({ summary: 'get index data', description: 'index page' })
+  @ApiResponse({ status: 200, description: 'Hello World', type: String })
   getHello(): string {
     return this.appService.getHello();
   }
--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3003);
+  const app = await NestFactory.create(AppModule, {
+    cors: { origin: 'http://localhost:3002', credentials: true },
+  });
+  app.setGlobalPrefix('/api/v1');
+
+  const config = new DocumentBuilder()
+    .setTitle('Resellers')
+    .setDescription('SIDE PROJECT - Resell System')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/v1/docs', app, document); //swagger 접속 주소 설정
+
+  await app.listen(3005);
 }
 bootstrap();
